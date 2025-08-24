08a5ffea171cdc7aebcea8daffdfbecc92962676
Yashpalsingh pawara <135001156+yashu1412@users.noreply.github.com>
2025-08-25 00:29:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,14 +16,10 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow all origins while still supporting credentials (cookies)
-      callback(null, origin || true)
-    },
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Vite's default port
     credentials: true,
-  })
+  }),
 )
-
 app.use(express.json())
 app.use(cookieParser())
 
